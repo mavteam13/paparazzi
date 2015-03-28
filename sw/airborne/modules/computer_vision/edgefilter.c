f6293c5ebfbf6807e9e53bfceb45fb40da954a38
k.n.mcguire@tudelft.nl <k.n.mcguire@tudelft.nl>
2015-03-28 15:49:02 +0100

    Added stereo vision to work with navigation and it worksgit status

--- sw/airborne/modules/computer_vision/edgefilter.c
+++ sw/airborne/modules/computer_vision/edgefilter.c
@@ -148,7 +148,7 @@ int detect_vertical_lines(struct img_struct *input, struct img_struct *output ,u
     int safe_heading_disp;
 
     uint8_t pxlcnt_temp;
-   // uint8_t pxlcnt_lines_bin[input->w];
+    // uint8_t pxlcnt_lines_bin[input->w];
 
     for(uint16_t x = 0;x<input->w; x++) {
         pxlcnt_lines[x]=0;
@@ -171,156 +171,205 @@ int detect_vertical_lines(struct img_struct *input, struct img_struct *output ,u
 
         }//else pxlcnt_lines[x]=0;
     }
-   // for(uint16_t x = 0;x<input->w; x++) pxlcnt_lines_bin[x]=0;
+    for(uint16_t x = 0;x<input->w; x++) pxlcnt_lines_bin[x]=0;
+     int Disp=0;
 
     for(uint16_t x = 0;x<input->w; x++) {
 
         if(pxlcnt_lines[x]>thres_disparity){
-            int D=multiply_disparity*pxlcnt_lines[x];
-            printf(" disparity is %d\n",D);
-            for(int r=-D;r<D;r++)
+            /*if(pxlcnt_lines[x]<25) Disp=(int)(0.5*pxlcnt_lines[x]);
+            if(pxlcnt_lines[x]<50) Disp=(int)(1*pxlcnt_lines[x]);
+            if(pxlcnt_lines[x]<75) Disp=(int)(2*pxlcnt_lines[x]);
+            if(pxlcnt_lines[x]>=75) Disp=(int)(3*pxlcnt_lines[x]);*/
+            Disp=(int)(multiply_disparity*pxlcnt_lines[x]);
+
+            printf(" disparity is %d\n",Disp);
+            for(int r=Disp;r<Disp;r++)
             {
                 if(x+r>=0 && x+r<input->w){
                     pxlcnt_lines_bin[x+r]=1;}
 
-                }
             }
-
-
         }
 
-        for(uint16_t y = 0; y < input->h; y++) {
-            for(uint16_t x = 0; x < input->w; x++) {
-                uint32_t idx = input->w*y*2 + (x)*2;
 
-                if(pxlcnt_lines_bin[x]==1){
-                    dest[idx+1]=255;
-                }else     dest[idx+1]=0;
+    }
 
-                dest[idx]=127;//source[idx+1];
-            }
+    for(uint16_t y = 0; y < input->h; y++) {
+        for(uint16_t x = 0; x < input->w; x++) {
+            uint32_t idx = input->w*y*2 + (x)*2;
 
+            if(pxlcnt_lines_bin[x]==1){
+                dest[idx]=255;
+            }else     dest[idx]=0;
+
+            dest[idx+1]=source[idx+1];
         }
 
-        //search right
-        int safe_heading_disp_left=-45;
+    }
 
-        for(uint16_t x=(input->w)/2;x>=0;x--)
-        {if(pxlcnt_lines_bin[x]==0){
-                safe_heading_disp_left=-(input->w/2-x)/4;
+    //search right
+    int D=20;
+    int safe_heading_disp_left=-90;
+    uint8_t safe_region_reached=0;
+
+
+    for(uint16_t x=(input->w)/2;x>=D;x--){
+        for(uint16_t r=0;r<D;r++){
+            if(pxlcnt_lines_bin[x-r]==0){
+                //printf("r is safe?\n");
+
+                safe_region_reached=1;
+            }
+
+            else {
+                safe_region_reached=0;
+                //  printf("r is not safe....\n");
                 break;
-            }}
-        int safe_heading_disp_right=45;
+            }
 
-        for(uint16_t x=0;x<(input->w)/2;x++)
-        {if(pxlcnt_lines_bin[x]==0){
-                safe_heading_disp_right=-(input->w/2-x)/4;
+        }
+        if(safe_region_reached==1){
+            //printf("safe_region_reached\n");
+            safe_heading_disp_left=-(input->w/2-x+D/2)/4;
+
+            break;
+        }
+    }
+
+
+    int safe_heading_disp_right=90;
+    safe_region_reached=0;
+
+    for(uint16_t x=(input->w)/2;x<(input->w)-D;x++)
+    {
+        for(uint16_t r=0;r<D;r++){
+            if(pxlcnt_lines_bin[x+r]==0)
+            {
+                safe_region_reached=1;
+              //  printf("r is safe? %d\n",pxlcnt_lines_bin[x+r]);
+            }
+
+            // break;
+            else  {
+              //  printf("r is not safe....%d\n",pxlcnt_lines_bin[x+r]);
+
+                safe_region_reached=0;
                 break;
-            }}
-
-        if (abs(safe_heading_disp_left)<abs(safe_heading_disp_right))
-        {
-            safe_heading_disp=safe_heading_disp_left;
-        }else{
-            safe_heading_disp=safe_heading_disp_right;
-        }
-
-        return safe_heading_disp;
-
-    }
-    void image_flow(struct img_struct *input,struct img_struct *input_prev,struct img_struct *output,double increment_value)
-    {
-
-
-        uint8_t *source = input->buf;
-        uint8_t *source_prev = input_prev->buf;
-        uint8_t *dest = output->buf;
-        uint8_t value=0;
-
-        for(uint16_t y = 0; y < input->h; y++) {
-            for(uint16_t x = 0; x < input->w; x++) {
-                uint32_t idx = input->w*y*2 + (x)*2;
-                if (source[idx+1]>10)
-                    value=(uint8_t)(increment_value*source_prev[idx+1]+5*source[idx+1]);
-                else value=(increment_value*source_prev[idx+1]+0);
-                if( value>254) value= 255;
-                if( value<0) value= 0;
-                //else dest[idx+1]=value;
-                dest[idx+1]=value;
-                dest[idx]=127;
             }
+
+        }
+        if(safe_region_reached==1){
+      //      printf("safe_region_reached\n");
+
+            safe_heading_disp_right=-(input->w/2-x-D/2)/4;
+            break;
         }
     }
 
-
-
-
-
-
-    int pixelcount(struct img_struct* input, uint32_t* pxcnt, uint8_t pxcnt_size)
+    if (abs(safe_heading_disp_left)<abs(safe_heading_disp_right))
     {
-
-        //uint32_t pixelcount[5]={0};
-        uint8_t idx_count;
-        uint8_t *source = input->buf;
-        uint8_t value=0;
-        int pxcnt_tot=0;
-
-
-
-        for(uint16_t y = 0; y < input->h; y++) {
-            idx_count=0;
-            for(uint16_t x = 0; x < input->w; x++) {
-                uint32_t idx = input->w*y*2 + (x)*2;
-                value=source[idx+1];
-
-                if (x%(input->w/(pxcnt_size))==0){
-                    idx_count++;
-                }
-                // printf("%d",idx_count);
-
-                if(value==255){
-                    pxcnt[idx_count-1]=pxcnt[idx_count-1]+1;
-                    pxcnt_tot++;
-                }
-
-
-
-
-            }
-        }
-        return pxcnt_tot;
+        safe_heading_disp=safe_heading_disp_left;
+    }else{
+        safe_heading_disp=safe_heading_disp_right;
     }
 
-    int pixelratio(struct img_struct* input, uint32_t* pxcnt, uint8_t pxcnt_size)
-    {
+    return safe_heading_disp;
 
-        //uint32_t pixelcount[5]={0};
-        uint8_t idx_count;
-        uint8_t *source = input->buf;
-        uint8_t value=0;
-        int pxcnt_tot=0;
+}
+void image_flow(struct img_struct *input,struct img_struct *input_prev,struct img_struct *output,double increment_value)
+{
 
 
+    uint8_t *source = input->buf;
+    uint8_t *source_prev = input_prev->buf;
+    uint8_t *dest = output->buf;
+    uint8_t value=0;
 
-        for(uint16_t y = 0; y < input->h; y++) {
-            idx_count=0;
-            for(uint16_t x = 0; x < input->w; x++) {
-                uint32_t idx = input->w*y*2 + (x)*2;
-                value=source[idx+1];
-
-                if (x%(input->w/(pxcnt_size))==0){
-                    idx_count++;
-                }
-                //printf("%d",idx_count);
-
-                pxcnt[idx_count-1]=pxcnt[idx_count-1]+value;
-                pxcnt_tot=pxcnt_tot+value;
-
-
-
-
-            }
+    for(uint16_t y = 0; y < input->h; y++) {
+        for(uint16_t x = 0; x < input->w; x++) {
+            uint32_t idx = input->w*y*2 + (x)*2;
+            if (source[idx+1]>10)
+                value=(uint8_t)(increment_value*source_prev[idx+1]+5*source[idx+1]);
+            else value=(increment_value*source_prev[idx+1]+0);
+            if( value>254) value= 255;
+            if( value<0) value= 0;
+            //else dest[idx+1]=value;
+            dest[idx+1]=value;
+            dest[idx]=127;
         }
-        return pxcnt_tot;
     }
+}
+
+
+
+
+
+
+int pixelcount(struct img_struct* input, uint32_t* pxcnt, uint8_t pxcnt_size)
+{
+
+    //uint32_t pixelcount[5]={0};
+    uint8_t idx_count;
+    uint8_t *source = input->buf;
+    uint8_t value=0;
+    int pxcnt_tot=0;
+
+
+
+    for(uint16_t y = 0; y < input->h; y++) {
+        idx_count=0;
+        for(uint16_t x = 0; x < input->w; x++) {
+            uint32_t idx = input->w*y*2 + (x)*2;
+            value=source[idx+1];
+
+            if (x%(input->w/(pxcnt_size))==0){
+                idx_count++;
+            }
+            // printf("%d",idx_count);
+
+            if(value==255){
+                pxcnt[idx_count-1]=pxcnt[idx_count-1]+1;
+                pxcnt_tot++;
+            }
+
+
+
+
+        }
+    }
+    return pxcnt_tot;
+}
+
+int pixelratio(struct img_struct* input, uint32_t* pxcnt, uint8_t pxcnt_size)
+{
+
+    //uint32_t pixelcount[5]={0};
+    uint8_t idx_count;
+    uint8_t *source = input->buf;
+    uint8_t value=0;
+    int pxcnt_tot=0;
+
+
+
+    for(uint16_t y = 0; y < input->h; y++) {
+        idx_count=0;
+        for(uint16_t x = 0; x < input->w; x++) {
+            uint32_t idx = input->w*y*2 + (x)*2;
+            value=source[idx+1];
+
+            if (x%(input->w/(pxcnt_size))==0){
+                idx_count++;
+            }
+            //printf("%d",idx_count);
+
+            pxcnt[idx_count-1]=pxcnt[idx_count-1]+value;
+            pxcnt_tot=pxcnt_tot+value;
+
+
+
+
+        }
+    }
+    return pxcnt_tot;
+}
